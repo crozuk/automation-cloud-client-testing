54498cacc13a6138302faa3c4fb40c934e1da507
Richard Crosby <crozuk@gmail.com>
2021-10-10 17:14:58 +0100

    api endpoints using ac-client (fixed inputs)

---
+++ test2.js
@@ -0,0 +1,92 @@
+// Enviroment variables
+require('dotenv').config();
+const app_secret = process.env.secret;
+
+//Express server
+var express = require('express');
+var app = express();
+const port = 3001;
+
+//Automation Cloud client
+const { Client } = require('@automationcloud/client');
+
+// Create client instance
+const client = new Client({
+    serviceId: "20ea0e52-1c0d-41ba-9ed2-4b50ca847f31",
+    category: "test",
+    auth: app_secret
+});
+
+//Create new job
+async function createJob() {
+    //Create job
+    const job = await client.createJob();
+    //console.log(job);
+    job.onStateChanged(newState => console.log(`job: ${newState}`));
+    return Promise.resolve(job);
+}
+
+//Submit input
+async function submitInput(data, job) {
+    var searchInput = data;
+    console.log(searchInput);
+    await job.submitInput('search_term', searchInput);
+    return Promise.resolve(job);
+}
+
+//Submit input
+async function submitSelected(data, job) {
+    var selectedTitle = data;
+    var selectedItem = {'title': selectedTitle};
+    console.log(selectedItem);
+    
+    //Wait for output
+    await job.waitForOutputs('SearchResults');
+    //Submit input
+    job.submitInput('selected_site', selectedItem);
+    
+    //Wait for completion
+    await job.waitForCompletion();
+    
+    return Promise.resolve(job);
+}
+
+//Create job with input
+async function createSubmit(term) {
+    job = await createJob();
+    await submitInput(term, job);
+    //Output
+    await job.waitForOutputs('SearchResults');
+    var output = await job.getOutput('SearchResults');
+    //console.log(output);
+    return job, output;
+}
+
+//Input select item to job
+async function selectItem(item) {
+    await submitSelected(item, job);
+    return job;
+}
+
+
+//Search endpoint
+app.post('/search', function (req, res) {
+    console.log("/search endpoint");
+    (async function(){
+        var submit = await createSubmit("apples");
+        console.log(submit);
+        res.send(submit);
+    })();
+})
+
+//Selected endpoint
+app.post('/selected', function (req, res) {
+    console.log("/selected endpoint");
+    selectItem("Apple - Wikipedia");
+    res.send("Selected...");
+})
+
+//Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
