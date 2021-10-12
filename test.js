0d6dcfbede0cb14a856b271aec342b9f95b2e916
Richard Crosby <crozuk@gmail.com>
2021-10-12 19:14:28 +0100

    test script

--- test.js
+++ test.js
@@ -1,18 +1,2 @@
-//Basic async functions
-async function getData() {
-  return Promise.resolve('this is');
-}
-
-async function getMoreData(data) {
-  return Promise.resolve(data + ' ' + 'more data');
-}
-
-async function getAll() {
-  const data = await getData();
-  const moreData = await getMoreData(data);
-  return moreData;
-}
-
-getAll().then((all) => {
-  console.log(all)
-})
+x = "Hello World";
+console.log(x);
