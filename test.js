429097ca2ebfef2b6e7cf1f5d4e08debdc9643e4
Richard Crosby <crozuk@gmail.com>
2021-10-17 12:23:49 +0100

    working base64 screenshot from job

--- test.js
+++ test.js
@@ -1,2 +1,37 @@
-x = "Hello World";
-console.log(x);
+// Enviroment variables
+require('dotenv').config();
+const appSecret = process.env.secret;
+// Testing variables
+const jobId = 'ccf7d6a9-1bb5-4b50-be27-97a6bae09a61';
+// Asios
+const axios = require('axios');
+
+async function getScreenshots (jobId) {
+    const username = appSecret;
+    const password = null;
+    //const data = null;
+    const token = Buffer.from(`${username}:${password}`, 'utf8').toString('base64');
+    var url = "https://api.automationcloud.net/jobs/" + jobId + "/screenshots";
+    //console.log(token);
+    //console.log(url);
+    var response = await axios.get(url, {
+        headers: {
+            'Authorization': `Basic ${token}`
+        },
+    })
+    var body = await response.data;
+    //console.log(body);
+    const latestScreenshot = body.data.reverse()[0];
+    //console.log(latestScreenshot);
+    var url = "https://api.automationcloud.net" + latestScreenshot.url
+    var response = await axios.get(url, {
+        responseType: 'arraybuffer',
+        headers: {
+            'Authorization': `Basic ${token}`
+        },
+    });
+    const imageBase64 = Buffer.from(response.data, 'binary').toString('base64');
+    console.log(imageBase64);
+    return imageBase64;
+}
+getScreenshots(jobId);
